54903daf973c89e1073a7f29abe303a2cdaafb91
duarte-phy <asduarte@asu.edu>
2025-12-15 18:57:55 -0700

    test: json config

--- main.py
+++ main.py
@@ -6,11 +6,14 @@ from pyscript.web import page, when
 
 ## Temporary Message
 message = "hello worl"
+x = np.array([1,123])
 
 #for terminal
 print(message)
+print(x * 2)
 
 #for page display
 display(message)
+display(x * 2)
 
 ## Create script that cleans html
